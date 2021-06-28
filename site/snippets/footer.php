0b4899e576027005e74dd1f55d639495bbed06a3
Jeanette de Waard <jeanette@newstory.nl>
2021-06-28 11:53:58 +0200

    added phpscss

---
+++ site/snippets/footer.php
@@ -0,0 +1,15 @@
+<footer>
+<div class="container-xl">
+    <div class="row">
+        <div class="col-md-6"><?php snippet('footer/menu') ?></div>
+        <div class="col-md-6 text-end"><?php snippet('footer/social') ?></div>
+    </div>
+    <div class="row">
+        <div class="col-md-6"><?php snippet('forms/subscribe-newsletter') ?></div>
+    </div>
+</div>
+</footer>
+
+<?php snippet('assets/js') ?>
+
+</body></html>
---
+++ site/snippets/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="<?= $kirby->language()->code(); ?>">
+<head>
+    <title><?= $page->title() ?> - <?= $site->title(); ?></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php snippet('scss') ?>
+    <?= css('assets/css/default.css') ?>
+</head>
+
+<body>
+
+<?php snippet('menu') ?>

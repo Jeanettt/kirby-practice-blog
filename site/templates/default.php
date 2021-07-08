e331b4fb21ecd48621fca13d9c13a913980f833b
Jeanette de Waard <jeanette@newstory.nl>
2021-07-08 13:57:03 +0200

    fixed collection narrow layout

--- site/templates/default.php
+++ site/templates/default.php
@@ -6,17 +6,17 @@
                 <!-- <h1 class="heading-group__headline"><?= $page->title() ?></h1> -->
             </div>
         </header>
-    </div>
-    <div class="simple-layout__main">
-        <div class="rich-text rich-text--centered">
-            <h2>Dummy text</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+        <div class="simple-layout__main">
+            <div class="rich-text rich-text--centered">
+                <h2>Dummy text</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
         </div>
     </div>
 </section>
-<section class="simple-layout">
+<section class="simple-layout simple-layout--centered-text simple-layout--max-narrow-width">
     <div class="simple-layout__body">
         <div class="simple-layout__header">
             <h2>Laatste blogs</h2>
